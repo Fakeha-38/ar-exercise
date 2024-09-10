05d38043a62f60a1683a133f9200d9bfc5ea3564
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-09-10 08:14:14 -0400

    Create a new store using data entered by the user

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter a store name: "
+store_name = gets.chomp
+print "Enter a annual revenue:"
+new_annual_revenue = gets.chomp
+print "Store has mens apparel: "
+men_apparel = gets.chomp
+print "Store has womens apparel: "
+women_apparel = gets.chomp
+
+# Attempt to create the store with only the name provided
+new_store = Store.new(
+  name: store_name,
+  annual_revenue: new_annual_revenue,
+  mens_apparel: men_apparel,
+  womens_apparel: women_apparel
+)
+if new_store.save
+  puts "Store '#{store_name}' created successfully."
+else
+  puts "Failed to create store. Errors: "
+  new_store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+end
