23080e0f46ec22c3dda822537eded467d7162fcb
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-09-10 06:26:49 -0400

    Filter data from tables. Like finding total stores generating m revenue or all mens stores etc.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum(:annual_revenue)
+puts "The revenue of th ecompany fromm all stores is: #{@sum}"
+puts "The average annual revenue for all stores is: #{@sum/Store.count}"
+@stores_genersting_1m = Store.where('annual_revenue >= ?', 1_000_000).count
+
+puts "Number of stores generating $1M & more: #{@stores_genersting_1m}"
