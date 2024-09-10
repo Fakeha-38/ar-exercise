67e8ab8c6cdbe0c9eb39c8850fa1ac9445f4977d
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-09-10 06:55:43 -0400

    Add mock data for employees in relaton to the stores they work in

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,21 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store4 = Store.find(4)
+@store5 = Store.find(5)
+@store6 = Store.find(6)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Eve", last_name: "Adams", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Alice", last_name: "Smith", hourly_rate: 50)
+@store2.employees.create(first_name: "Frank", last_name: "White", hourly_rate: 58)
+
+@store4.employees.create(first_name: "Bob", last_name: "Johnson", hourly_rate: 55)
+@store4.employees.create(first_name: "Grace", last_name: "Hopper", hourly_rate: 70)
+
+@store5.employees.create(first_name: "Charlie", last_name: "Brown", hourly_rate: 45)
+@store5.employees.create(first_name: "Henry", last_name: "Ford", hourly_rate: 48)
+
+@store6.employees.create(first_name: "Dana", last_name: "Scully", hourly_rate: 65)
+@store6.employees.create(first_name: "Ivy", last_name: "Williams", hourly_rate: 52)
